32ec705ed3c7f3afa70aa93dfb9027e7629402a9
gloria988 <m15802022402@163.com>
2020-07-14 18:55:43 +0800

    修改了一点错误

--- src/javabean/mysql_search.java
+++ src/javabean/mysql_search.java
@@ -6,15 +6,15 @@ import java.sql.SQLException;
 
 public class mysql_search {
 	private PreparedStatement pstm;     	 
-	private Connection conn;
+	private Connection conn;	
 	//搜索填空题相关记录
 	//搜索的那个字段是int类型
-	public void  blank_getInt(int ziduan,int blankid) {
+	public void  blank_getInt(int ziduan,int id) {
 		try {
 			 mysql_DB db=new mysql_DB();
 				conn=db.connectDB();
-				pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_blank where "+blankid+" =?");
-				pstm.setInt(1, ziduan);
+				pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_blank where blankid =?");
+				pstm.setInt(1, id);
 				pstm.executeUpdate();
 				db.close(conn);
 			}catch(SQLException ex){
@@ -22,26 +22,27 @@ public class mysql_search {
 			}
 	}
 	//搜索的那个字段是String类型
-	public void  blank_getString(String ziduan,int blankid) {
+	public void  blank_getString(String ziduan,int id) {
 			try {
 				 mysql_DB db=new mysql_DB();
 					conn=db.connectDB();
-					pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_blank where "+blankid+" =?");
-					pstm.setString(1, ziduan);
+					pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_blank where blankid =?");
+					pstm.setInt(1, id);
 					pstm.executeUpdate();
 					db.close(conn);
 				}catch(SQLException ex){
 				ex.printStackTrace();
 				}
 	}
+	
 	//搜索判断题相关记录
 	//搜索的那个字段是int类型
-	public void  judgement_getInt(int ziduan,int judgementid) {
+	public void  judgement_getInt(int ziduan,int id) {
 		try {
 			 mysql_DB db=new mysql_DB();
 				conn=db.connectDB();
-				pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_judgement where "+judgementid+" =?");
-				pstm.setInt(1, ziduan);
+				pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_judgement where judgementid =?");
+				pstm.setInt(1, id);
 				pstm.executeUpdate();
 				db.close(conn);
 			}catch(SQLException ex){
@@ -49,26 +50,27 @@ public class mysql_search {
 			}
 	}
 	//搜索的那个字段是String类型
-	public void  judgement_getString(String ziduan,int judgementid) {
+	public void  judgement_getString(String ziduan,int id) {
 			try {
 				 mysql_DB db=new mysql_DB();
 					conn=db.connectDB();
-					pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_judgement where "+judgementid+" =?");
-					pstm.setString(1, ziduan);
+					pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_judgement where judgementid =?");
+					pstm.setInt(1, id);
 					pstm.executeUpdate();
 					db.close(conn);
 				}catch(SQLException ex){
 				ex.printStackTrace();
 				}
 	}
+	
 	//搜索多选题相关记录
 	//搜索的那个字段是int类型
-	public void  multi_getInt(int ziduan,int multiid) {
+	public void  multi_getInt(int ziduan,int id) {
 		try {
 			 mysql_DB db=new mysql_DB();
 				conn=db.connectDB();
-				pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_multi where "+multiid+" =?");
-				pstm.setInt(1, ziduan);
+				pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_multi where multiid =?");
+				pstm.setInt(1, id);
 				pstm.executeUpdate();
 				db.close(conn);
 			}catch(SQLException ex){
@@ -76,26 +78,27 @@ public class mysql_search {
 			}
 	}
 	//搜索的那个字段是String类型
-	public void  multi_getString(String ziduan,int multiid) {
+	public void  multi_getString(String ziduan,int id) {
 			try {
 				 mysql_DB db=new mysql_DB();
 					conn=db.connectDB();
-					pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_multi where "+multiid+" =?");
-					pstm.setString(1, ziduan);
+					pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_multi where multiid =?");
+					pstm.setInt(1, id);
 					pstm.executeUpdate();
 					db.close(conn);
 				}catch(SQLException ex){
 				ex.printStackTrace();
 				}
 	}
+	
 	//搜索单选题相关记录
 	//搜索的那个字段是int类型
-	public void  single_getInt(int ziduan,int singleid) {
+	public void  single_getInt(int ziduan,int id) {
 		try {
 			 mysql_DB db=new mysql_DB();
 				conn=db.connectDB();
-				pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_single where "+singleid+" =?");
-				pstm.setInt(1, ziduan);
+				pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_single where singleid =?");
+				pstm.setInt(1, id);
 				pstm.executeUpdate();
 				db.close(conn);
 			}catch(SQLException ex){
@@ -103,12 +106,12 @@ public class mysql_search {
 			}
 	}
 	//搜索的那个字段是String类型
-	public void  single_getString(String ziduan,int singleid) {
+	public void  single_getString(String ziduan,int id) {
 			try {
 				 mysql_DB db=new mysql_DB();
 					conn=db.connectDB();
-					pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_single where "+singleid+" =?");
-					pstm.setString(1, ziduan);
+					pstm=conn.prepareStatement("select"+ziduan+"from kaoshi_single where singleid =?");
+					pstm.setInt(1, id);
 					pstm.executeUpdate();
 					db.close(conn);
 				}catch(SQLException ex){
