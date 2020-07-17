4d4edc5bc373144f84e3070acac3fc0bf136366e
gloria988 <m15802022402@163.com>
2020-07-17 18:00:48 +0800

    计算试卷总得分

---
+++ src/javabean/mysql_update_goal.java
@@ -0,0 +1,33 @@
+package javabean;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class mysql_update_goal {
+	private PreparedStatement pstm;     
+	private Connection conn;
+	private ResultSet rs;
+	
+	public void goal_update(int pid) {
+		int sum=0;
+		try {
+			 mysql_DB db=new mysql_DB();
+				conn=db.connectDB();
+				pstm=conn.prepareStatement("select sum(goal) from TestInf");
+				rs=pstm.executeQuery();
+				while(rs.next()) {
+					sum=rs.getInt(1);
+				}
+				
+				pstm=conn.prepareStatement("update StuTest set goal =? where pid =?");
+				pstm.setInt(1, sum);
+				pstm.setInt(2, pid);
+				pstm.executeUpdate();				
+				db.close(conn);
+			}catch(SQLException ex){
+			ex.printStackTrace();
+			}
+	}
+}
