5ef038efcce8abe079385abd06496a82680906ab
Hiltoness <hiltoness@163.com>
2020-07-20 00:15:33 +0800

    增加了三个类
    已出题目tlist，刷题题目内容id_type，答卷题目内容tpoic answer

---
+++ src/javabean/Tlist.java
@@ -0,0 +1,21 @@
+package javabean;
+/*
+ * 已出题目列表
+ */
+public class Tlist {
+	private int id;
+	private String type;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
--- src/javabean/Topic_Answer.java
+++ src/javabean/Topic_Answer.java
@@ -1,6 +1,8 @@
 package javabean;
-
-public class id_type {
+/*
+ * 题目内容、作答结果、答案
+ */
+public class Topic_Answer {
 	private int id;
 	private String type;
 	private String title;
@@ -8,64 +10,48 @@ public class id_type {
 	private String select_B;
 	private String select_C;
 	private String select_D;
-	private String value;
-
-	//	public id_type(int id,String type) {
-//		super();
-//		this.id=id;
-//		this.type=type;
-//		
-//		
-//	}
-//	
+	private String value;//作答结果
+	private String answer;//答案
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
-		this.id=id;
+		this.id = id;
 	}
 	public String getType() {
 		return type;
 	}
 	public void setType(String type) {
-		this.type=type;
+		this.type = type;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getSelect_A() {
 		return select_A;
 	}
-
 	public void setSelect_A(String select_A) {
 		this.select_A = select_A;
 	}
-
 	public String getSelect_B() {
 		return select_B;
 	}
-
 	public void setSelect_B(String select_B) {
 		this.select_B = select_B;
 	}
-
 	public String getSelect_C() {
 		return select_C;
 	}
-
 	public void setSelect_C(String select_C) {
 		this.select_C = select_C;
 	}
-
 	public String getSelect_D() {
 		return select_D;
 	}
-
 	public void setSelect_D(String select_D) {
 		this.select_D = select_D;
 	}
@@ -75,29 +61,12 @@ public class id_type {
 	public void setValue(String value) {
 		this.value = value;
 	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj){				
-			return true;
-		}
-		if (obj == null){
-			return false;				
-		}
-		if (getClass() != obj.getClass()){				
-			return false;
-		}
-		id_type sub = (id_type) obj;
-		if (sub.getType() == null) {
-			return false;
-		}
-		if (type == null) {
-			if (sub.getType() != null){					
-				return false;
-			}
-		} else if (!type.equals(sub.getType())){				
-			return false;
-		}
-		return true;
+	public String getAnswer() {
+		return answer;
 	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
 
 }
